60c08cc0bd23d29dc35b21d6085a6ce7f151df02
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-01-07 17:10:11 +0400

    updatedWithFor

--- js/app.js
+++ js/app.js
@@ -7,21 +7,53 @@ const users = [
         "address": "Usa",
         "phone": "555-785-99-68",
         "birthDate": "01.01.2000"
-    }
+    },
+    {
+        "firstName": "Terry",
+        "lastName": "Fry",
+        "age": 22,
+        "mail": "terry@example.com",
+        "address": "Usa",
+        "phone": "444-56-85-89",
+        "birthDate": "01.01.2020"
+    },
 ];
 
 let bodyTr = "";
 let headTr = "";
 
-for(key in users[0]){
-
-    headTr += `<tr><th>${key}</th></tr>`;
-    
-    for(val of users){
-        bodyTr += `<tr><td>${val[key]}</td></tr>`;
-    }
+for (key in users[0]) {
+    headTr += `<th>${key}</th>`;
 }
+
+// for (key in users[0]) {
+//     headTr = `<th>${key}</th>`;
+
+//     for(value of users){
+//         bodyTr += `<td>${value}</td>`;
+//     }
+// }
 
 const root = document.querySelector("#root");
 
-root.innerHTML = `<table><thead>${headTr}</thead><tbody>${bodyTr}</tbody></table>`;
+root.innerHTML =
+    `
+                        <table class="table table-striped table-bordered table-hover text-capitalize">
+                            <thead>
+                                <tr>${headTr}</tr>
+                            </thead>
+                            <tbody>
+                                <tr>${bodyTr}</tr>
+                            </tbody>
+                        </table>
+                    `;
+
+for (key in users[0]) {
+    for (let i = 0; i < users.length; i++) {
+
+        // console.log(users[i].key);
+
+        bodyTr += users[i].key;
+
+    }
+}
