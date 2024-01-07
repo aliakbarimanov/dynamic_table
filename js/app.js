e0ff5788dd38aedfc47c468a1ff4efd73ee3ac9b
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-01-08 02:00:46 +0400

    finished

--- js/app.js
+++ js/app.js
@@ -13,6 +13,60 @@ const users = [
         "lastName": "Fry",
         "age": 22,
         "mail": "terry@example.com",
+        "address": "GB",
+        "phone": "444-56-85-89",
+        "birthDate": "01.01.2020"
+    },
+    {
+        "firstName": "Jhon",
+        "lastName": "Doe",
+        "age": 40,
+        "mail": "jhon.doe@example.com",
+        "address": "GB",
+        "phone": "555-785-99-68",
+        "birthDate": "01.01.2000"
+    },
+    {
+        "firstName": "Terry",
+        "lastName": "Fry",
+        "age": 22,
+        "mail": "terry@example.com",
+        "address": "Usa",
+        "phone": "444-56-85-89",
+        "birthDate": "01.01.2020"
+    },
+    {
+        "firstName": "Jhon",
+        "lastName": "Doe",
+        "age": 40,
+        "mail": "jhon.doe@example.com",
+        "address": "Usa",
+        "phone": "555-785-99-68",
+        "birthDate": "01.01.2000"
+    },
+    {
+        "firstName": "Terry",
+        "lastName": "Fry",
+        "age": 22,
+        "mail": "terry@example.com",
+        "address": "GB",
+        "phone": "444-56-85-89",
+        "birthDate": "01.01.2020"
+    },
+    {
+        "firstName": "Jhon",
+        "lastName": "Doe",
+        "age": 40,
+        "mail": "jhon.doe@example.com",
+        "address": "GB",
+        "phone": "555-785-99-68",
+        "birthDate": "01.01.2000"
+    },
+    {
+        "firstName": "Terry",
+        "lastName": "Fry",
+        "age": 22,
+        "mail": "terry@example.com",
         "address": "Usa",
         "phone": "444-56-85-89",
         "birthDate": "01.01.2020"
@@ -26,34 +80,25 @@ for (key in users[0]) {
     headTr += `<th>${key}</th>`;
 }
 
-// for (key in users[0]) {
-//     headTr = `<th>${key}</th>`;
+for (let i = 0; i < users.length; i++) {
 
-//     for(value of users){
-//         bodyTr += `<td>${value}</td>`;
-//     }
-// }
+    bodyTr += `<tr></tr>`;
+
+    for (key in users[0]) {
+        bodyTr += `<td>${users[i][key]}</td>`;
+    }
+}
 
 const root = document.querySelector("#root");
 
 root.innerHTML =
     `
-                        <table class="table table-striped table-bordered table-hover text-capitalize">
+                        <table class="table table-bordered table-hover text-capitalize">
                             <thead>
                                 <tr>${headTr}</tr>
                             </thead>
                             <tbody>
-                                <tr>${bodyTr}</tr>
+                                ${bodyTr}
                             </tbody>
                         </table>
                     `;
-
-for (key in users[0]) {
-    for (let i = 0; i < users.length; i++) {
-
-        // console.log(users[i].key);
-
-        bodyTr += users[i].key;
-
-    }
-}
